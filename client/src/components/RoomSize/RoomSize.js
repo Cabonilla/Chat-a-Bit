35096fe68791d0ec630f84d4ef16f41bf4885aaf
cabonilla <christian.bonilla01@gmail.com>
2021-06-28 15:13:18 -0700

    Room visitor basic functionality done. I'll work on random colors later.

--- client/src/components/RoomSize/RoomSize.js
+++ client/src/components/RoomSize/RoomSize.js
@@ -1,10 +1,15 @@
+import { hexToCSSFilter } from 'hex-to-css-filter';
 import React from 'react';
 import onlineIcon from '../../icons/onlineIcon.svg';
 import './RoomSize.css';
 
 const RoomSize = ({ users }) => {
-	const randomHex = Math.floor(Math.random()*16777215).toString(16);
-	
+	const randomHex = "#" + Math.floor(Math.random()*16777215).toString(16)
+	const randomFilter = hexToCSSFilter(randomHex)
+	const cleanFilter = randomFilter.filter.slice(0, -1)
+	const finalFilter = "filter: " + cleanFilter
+	// randomFilter.filter = finalFilter
+
 	return (
   <div className="RoomSize">
     {
@@ -13,7 +18,7 @@ const RoomSize = ({ users }) => {
           <div className="currently-online">
             <div className="activeContainer">
                 {users.map(({name}) => (
-                  <div key={name} className="activeItem">
+                  <div key={name} onLoad={() => console.log(finalFilter)} className="activeItem">
                    	{name + " "}
                     <img src={onlineIcon} className="online" alt="Online Icon"/> 
                   </div>
