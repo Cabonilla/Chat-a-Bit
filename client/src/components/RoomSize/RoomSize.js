7febf43392bfa79239062332f98534027552e5b2
cabonilla <christian.bonilla01@gmail.com>
2021-06-28 15:14:34 -0700

    Cleaned up code for now.

--- client/src/components/RoomSize/RoomSize.js
+++ client/src/components/RoomSize/RoomSize.js
@@ -1,13 +1,12 @@
-import { hexToCSSFilter } from 'hex-to-css-filter';
 import React from 'react';
 import onlineIcon from '../../icons/onlineIcon.svg';
 import './RoomSize.css';
 
 const RoomSize = ({ users }) => {
-	const randomHex = "#" + Math.floor(Math.random()*16777215).toString(16)
-	const randomFilter = hexToCSSFilter(randomHex)
-	const cleanFilter = randomFilter.filter.slice(0, -1)
-	const finalFilter = "filter: " + cleanFilter
+	// const randomHex = "#" + Math.floor(Math.random()*16777215).toString(16)
+	// const randomFilter = hexToCSSFilter(randomHex)
+	// const cleanFilter = randomFilter.filter.slice(0, -1)
+	// const finalFilter = "filter: " + cleanFilter
 	// randomFilter.filter = finalFilter
 
 	return (
@@ -18,7 +17,7 @@ const RoomSize = ({ users }) => {
           <div className="currently-online">
             <div className="activeContainer">
                 {users.map(({name}) => (
-                  <div key={name} onLoad={() => console.log(finalFilter)} className="activeItem">
+                  <div key={name} className="activeItem">
                    	{name + " "}
                     <img src={onlineIcon} className="online" alt="Online Icon"/> 
                   </div>
